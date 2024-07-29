cc5511379e4770e82fd7858278410bdb331a975a
Amr Ali <amraliahmed25@gmail.com>
2024-07-30 01:32:05 +0300

    Update the JDK from 21 to 22

--- generate_allure_report.bat
+++ generate_allure_report.bat
@@ -1,6 +1,6 @@
 @echo off
 :: If you already have a valid JAVA_HOME environment variable set, feel free to comment the below two lines
-set JAVA_HOME=C:\Program Files\Java\jdk-22
+set JAVA_HOME=C:\Users\amral\.jdks\openjdk-22.0.2
 set path=%JAVA_HOME%\bin;%path%
 set path=C:\Users\amral\.m2\repository\allure\allure-2.26.0\bin;%path%
 allure serve allure-results -h localhost
